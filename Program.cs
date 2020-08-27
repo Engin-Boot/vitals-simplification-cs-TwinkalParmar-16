4210896d66a2b4fc57641f2a95ceb8074cc73279
Twinkal Parmar <66124568+TwinkalParmar-16@users.noreply.github.com>
2020-08-27 20:00:22 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -8,14 +8,14 @@ namespace Vital_Checks
         //The purpose is to treat without delay.
         static int Main(string[] args)
         {
-            ExpectedResult.ExpectTrue(VitalChecker.vitalsAreOk(100, 95, 60));
-            ExpectedResult.ExpectFalse(VitalChecker.vitalsAreOk(40, 91, 92));
-            ExpectedResult.ExpectFalse(VitalChecker.vitalsAreOk(50, 95, 75));
-            ExpectedResult.ExpectFalse(VitalChecker.vitalsAreOk(175, 95, 75));
-            ExpectedResult.ExpectTrue(VitalChecker.vitalsAreOk(90, 95, 75));
-            ExpectedResult.ExpectTrue(VitalChecker.vitalsAreOk(97, 100, 75));
-            ExpectedResult.ExpectFalse(VitalChecker.vitalsAreOk(101, 93, 20));
-            ExpectedResult.ExpectFalse(VitalChecker.vitalsAreOk(101, 100, 105));
+            ExpectedResult.ExpectTrue(checker.vitalsAreOk(100, 95, 60));
+            ExpectedResult.ExpectFalse(checker.vitalsAreOk(40, 91, 92));
+            ExpectedResult.ExpectFalse(checker.vitalsAreOk(50, 95, 75));
+            ExpectedResult.ExpectFalse(checker.vitalsAreOk(175, 95, 75));
+            ExpectedResult.ExpectTrue(checker.vitalsAreOk(90, 95, 75));
+            ExpectedResult.ExpectTrue(checker.vitalsAreOk(97, 100, 75));
+            ExpectedResult.ExpectFalse(checker.vitalsAreOk(101, 93, 20));
+            ExpectedResult.ExpectFalse(checker.vitalsAreOk(101, 100, 105));
             Console.WriteLine("All ok");
             return 0;
         }
---
+++ checker.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Vital_Checks
+{
+    // This class is responsible to detect when any of the body vitals go out of limits
+    class checker
+    {
+        public static bool vitalsAreOk(float bpm, float spo2, float respRate)
+        {
+            //True only when all the vitals is okay else false
+            if (RespRateChecker.isRespRateOkay(respRate) && SpO2Checker.isSpO2Okay(spo2) && BpmChecker.isBpmOkay(bpm))
+            {
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+    }
+}
